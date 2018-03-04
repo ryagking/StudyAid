2f82ddf6da86334d4d16fc3fabfaaeb2c8fe9d63
Ryan <kinger145@gmail.com>
2018-03-03 20:02:45 -0500

    added StudySession event

--- StudyAid/App.js
+++ StudyAid/App.js
@@ -1,23 +1,90 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View, StyleSheet, Button } from 'react-native';
+import t from 'tcomb-form-native'; // 0.6.9
+import { Dropdown } from 'react-native-material-dropdown';
 
-export default class App extends React.Component {
-  render() {
+const Form = t.form.Form;
+
+const StudySession = t.struct({
+  course: t.String,
+  title: t.maybe(t.String),
+  time: t.Number,
+  goal: t.String
+  });
+
+const formStyles = {
+  ...Form.stylesheet,
+  formGroup: {
+    normal: {
+      marginBottom: 10
+    },
+  },
+  controlLabel: {
+    normal: {
+      color: 'black',
+      fontSize: 18,
+      marginBottom: 7,
+      fontWeight: '600'
+    },
+    // the style applied when a validation error occours
+    error: {
+      color: 'black',
+      fontSize: 18,
+      marginBottom: 7,
+      fontWeight: '600'
+    }
+  }
+}
+
+const options = {
+  fields: {
+    course: {
+      label: 'Course',
+      error: 'Without an email address how are you going to reset your password when you forget it?'
+    },
+    title: {
+      label: 'Study session title',
+    },
+    time: {
+      label: 'Session length (minutes)',
+      error: 'Choose something you use on a dozen other sites or something you won\'t remember'
+    },
+  },
+  stylesheet: formStyles,
+};
+
+export default class App extends Component {
+  handleSubmit = () => {
+    const value = this._form.getValue();
+    console.log('value: ', value);
+  }
+
+  render()
+  {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Form
+          ref={c => this._form = c}
+          type={StudySession}
+          options={options}
+        />
+        <Button
+          title="Add"
+          onPress={this.handleSubmit}
+        />
       </View>
+
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create(
+{
+    container:
+    {
+        justifyContent: 'center',
+        marginTop: 50,
+        padding: 20,
+        backgroundColor: '#EB8686',
+    },
+  });
